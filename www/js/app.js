90330b152fa75fabcc2775e5a420474c3c661720
alexandrejuk <ale_santos.soares@hotmail.com>
2017-12-03 11:51:18 -0200

    remove: controller of app module

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,6 @@
 const app = angular.module('icalc', ['ionic']);
-app.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
+app.run($ionicPlatform => {
+  $ionicPlatform.ready(() => {
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
@@ -10,52 +10,3 @@ app.run(function($ionicPlatform) {
     }
   });
 });
-
-app.controller('calculatorCtrl', ($scope) => {
-
-  $scope.title = 'iCalc'
-  $scope.keyboard = [ 9, 8, 7, 6, 5, 4, 3, 2, 1, '.', 0, '='];
-  $scope.operators = [ '/', '*', '-', '+',];
-  $scope.printEquation = '0';
-  $scope.equationResult = '';
-  $scope.lastParamSave = '';
-
-  $scope.equation = (data) => {
-    if(data === '=') {
-      $scope.equationResult = eval( $scope.printEquation);
-    }else {
-      if(!isNaN(data) && data === 0 &&  $scope.printEquation === '0') {
-        $scope.lastParamSave = '';
-        $scope.printEquation = '0';
-      }else if(
-        $scope.printEquation === '0' &&
-        $scope.lastParamSave === '' &&
-        data !== '.'  &&
-        data !== '*'  &&
-        data !== '/'  &&
-        data !== '+'
-      ){
-        $scope.lastParamSave = data;
-        $scope.printEquation = data.toString();
-      }
-      else if (!isNaN(data) && !isNaN($scope.lastParamSave)) {
-        $scope.lastParamSave = data;
-        $scope.printEquation += data;
-      }
-      else if ($scope.lastParamSave !== data && !isNaN($scope.lastParamSave)) {
-        $scope.lastParamSave = data;
-        $scope.printEquation += data;
-      }
-      else if (!isNaN(data) && isNaN($scope.lastParamSave)) {
-        $scope.lastParamSave = data;
-        $scope.printEquation += data;
-      }
-    }
-  }
-
-  $scope.clean = () => {
-    $scope.printEquation = '0';
-    $scope.equationResult = '';
-    $scope.lastParamSave = '';
-  }
-})
